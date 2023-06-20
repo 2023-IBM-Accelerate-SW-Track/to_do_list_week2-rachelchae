13268d66c48f70246cea0d196e86c090e64d5ba6
RachelChae <rachel_chae@brown.edu>
2023-06-19 22:18:20 -0400

    all tests passing

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -7,14 +7,17 @@ class AddTodo extends Component {
     super();
     this.state = {
       content: "",
+      date: "",
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user.
   // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
   // into the text field.
+  
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
+      date: Date().toLocaleString('en-US')
     });
   };
   // The handleSubmit function collects the forms input and puts it into the react state.
@@ -27,6 +30,7 @@ class AddTodo extends Component {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
+        date: ""
       });
     }
   };
